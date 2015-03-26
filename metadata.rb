f3b4145e2474e95e9f3128d212fe3c05d244d048
Andrew DuFour <andy.k.dufour@gmail.com>
2015-03-26 01:16:28 -0400

    Bumped to Version 0.11.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andy.k.dufour@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures jboss7'
 long_description 'Installs/Configures jboss7'
-version '0.11.1'
+version '0.11.2'
 
 depends 'apt'
 depends 'java'
